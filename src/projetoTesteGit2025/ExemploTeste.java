6183fc1cbf608dd448d5a3870d397a05adf5da81
facurymanoel <mffacuri@terra.com.br>
2025-08-24 14:59:23 -0300

    segundo commit

--- src/projetoTesteGit2025/ExemploTeste.java
+++ src/projetoTesteGit2025/ExemploTeste.java
@@ -4,6 +4,10 @@ public class ExemploTeste {
 
 	public static void main(String[] args) {
 		 System.out.println("Hello World 2025!!!!");
+		 
+		 for(int i = 1; i <= 5; i++ ) {
+			 System.out.println("Valor = " + i);
+		 }
 
 	}
 
